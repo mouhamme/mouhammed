123095c754645ede5bf6aa5d47d6e2c761005bf0
mouhamme <46679476+mouhamme@users.noreply.github.com>
2020-12-06 21:56:24 +0300

    Update .env.js

--- .env.js
+++ .env.js
@@ -1,5 +1,6 @@
-token= Nzg1MDkyNzk2NTMyOTE2MjM0.X8y0pQ.HUFrzpBbGQSeCUcFHcuLDOx_5Z0
-prefix= +
-owner= 519855318147006464
-stayinchannel= true
-updatealerts= false
+module.exports = {
+    TOKEN: 'Nzg1MDkyNzk2NTMyOTE2MjM0.X8y0pQ.Velihs3BiSGWdhSEaab2JN5Fpfw',    
+    YT_API_KEY: 'AIzaSyBJ9-xgfvzmF3iq6ee9L58ksSdcS0zAWOQ', 
+    prefix: '+',
+    devs: ['']
+}
