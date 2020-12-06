e87f90f2784189dc493cbea009ca835bf1421acb
mouhamme <46679476+mouhamme@users.noreply.github.com>
2020-12-06 21:58:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get("/", (request, response) => {
 });
 app.listen(process.env.PORT);
 setInterval(() => {
-  http.get(`https://glitch.com/~mouhammed-`);
+  http.get(`https://mouhammed.glitch.me`);
 }, 280000);
 
 // كل البكجات الي ممكن تحتجها في اي بوت 
