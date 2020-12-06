a8cc3a20ba0d5232aacd8f144db7e7bfdf40a16c
mouhamme <46679476+mouhamme@users.noreply.github.com>
2020-12-06 21:03:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,75 @@
-var express = require("express");
-var http = require("http");
-var app = express();
-
-// Ping The Apps.
-app.use(express.static("public"));
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-    response.sendStatus(200);
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  response.sendStatus(200);
 });
-
-// Request Listeners.
-var listener = app.listen(process.env.PORT, function () {
-    console.log("Your app is listening on port " + listener.address().port);
-});
+app.listen(process.env.PORT);
 setInterval(() => {
-    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 270000);
+  http.get(`https://glitch.com/~mouhammed-`);
+}, 280000);
+
+// كل البكجات الي ممكن تحتجها في اي بوت 
+const { Client, RichEmbed } = require("discord.js");
+var { Util } = require('discord.js');
+const {TOKEN, YT_API_KEY, prefix, devs} = require('./config')
+const client = new Client({ disableEveryone: true})
+const ytdl = require("ytdl-core");
+const canvas = require("canvas");
+const Canvas = require("canvas");
+const convert = require("hh-mm-ss")
+const fetchVideoInfo = require("youtube-info");
+const botversion = require('./package.json').version;
+const simpleytapi = require('simple-youtube-api')
+const moment = require("moment");
+const fs = require('fs');
+const util = require("util")
+const gif = require("gif-search");
+const opus = require("node-opus");
+const ms = require("ms");
+const jimp = require("jimp");
+const { get } = require('snekfetch');
+const guild = require('guild');
+const dateFormat = require('dateformat');//npm i dateformat
+const YouTube = require('simple-youtube-api');
+const youtube = new YouTube('AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8');
+const hastebins = require('hastebin-gen');
+const getYoutubeID = require('get-youtube-id');
+const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
+const pretty = require("pretty-ms");
+client.login(TOKEN);
+const queue = new Map();
+var table = require('table').table
+const Discord = require('discord.js');
+client.on('ready', () => { 
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+ 
+
+
+//كود برودكاست
+
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : osama tech`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : osama tech ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`hello`,"http://twitch.tv/S-F") //باسم سيرفرك او اي شي تبيه hello تعديل مهم عدل كلمة 
+client.user.setStatus("dnd")
+});
